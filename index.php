1bdeeaabe67b98d514aa0a9b86caea83de028921
J. Harrison <blujinonlee@gmail.com>
2014-07-22 16:55:20 -0500

    Consolidated and Simplified Input Focusing
    Consolidated and simplified onEnter() method and added moveFocus() to
    handle changing focus better.

--- index.php
+++ index.php
@@ -62,54 +62,80 @@ $location = array("Clerk","Transportation Manager","Maintenance","Maintenance Ma
 	
 	
 	<script type="text/javascript">
-		var pcModels = ["Dell Optiplex 790"];
-		var printerModels = ["17in"];
-		var monitorModels = [];
-		var netPrinterModels = [];
-		
-		
-		<?php
-		
-		
-		$con = mysqli_connect("localhost", "root", "", "inventory");
-		
-		$printerModel;
-		$netPrinterModel;
-		
-		
-		$results = mysqli_query($con, "SELECT DISTINCT model from PCs ORDER BY Model");
-		
-		
-		while ($row = mysqli_fetch_array($results)){
-			
-			echo "pcModels.push('{$row[0]}'); \n";
-			
-		}
-		
-		$results = mysqli_query($con, "SELECT DISTINCT model from monitors ORDER BY Model");
-		while ($row = mysqli_fetch_array($results)){
-			
-			echo "monitorModels.push('{$row[0]}'); \n";
-			
-		}
-		
-		$results = mysqli_query($con, "SELECT DISTINCT model from Printers ORDER BY Model");
-		while ($row = mysqli_fetch_array($results)){
-			
-			echo "printerModels.push('{$row[0]}'); \n";
-			
-		}
-		
-		$results = mysqli_query($con, "SELECT DISTINCT model from netPrinters ORDER BY Model");
-		while ($row = mysqli_fetch_array($results)){
-			
-			echo "netPrinterModels.push('{$row[0]}'); \n";
-		}
-		
-		?>
+	var pcModels = ["Dell Optiplex 790"];
+	var printerModels = ["17in"];
+	var monitorModels = [];
+	var netPrinterModels = [];
 	
-	var scanForm = document.getElementById("scanForm");
+	<?php
 	
+	
+	$con = mysqli_connect("localhost", "root", "", "inventory");
+	
+	$printerModel;
+	$netPrinterModel;
+	
+	
+	$results = mysqli_query($con, "SELECT DISTINCT model from PCs ORDER BY Model");
+	
+	
+	while ($row = mysqli_fetch_array($results)){
+		
+		echo "pcModels.push('{$row[0]}'); \n";
+		
+	}
+	
+	$results = mysqli_query($con, "SELECT DISTINCT model from monitors ORDER BY Model");
+	while ($row = mysqli_fetch_array($results)){
+		
+		echo "monitorModels.push('{$row[0]}'); \n";
+		
+	}
+	
+	$results = mysqli_query($con, "SELECT DISTINCT model from Printers ORDER BY Model");
+	while ($row = mysqli_fetch_array($results)){
+		
+		echo "printerModels.push('{$row[0]}'); \n";
+		
+	}
+	
+	$results = mysqli_query($con, "SELECT DISTINCT model from netPrinters ORDER BY Model");
+	while ($row = mysqli_fetch_array($results)){
+		
+		echo "netPrinterModels.push('{$row[0]}'); \n";
+	}
+	
+	?>
+	
+	
+	//official <select> of pcs
+	var pcModelDropdown = "<select id='pcModel' name='pcModel' onchange='ifOtherPC()'>";
+	for(var i = 0; i < pcModels.length; i++){	
+		pcModelDropdown += "<option value='"+pcModels[i]+"'>"+pcModels[i]+"</options>";
+	}
+	pcModelDropdown += "<option value='other'>other</option></select><span id='newPCModelSpan'></span>";
+	
+	//official <select> of monitors
+	var monitorModelsDropdown = "<select name ='model' id='model' onchange='ifOther()'>";
+	for (var i = 0; i < monitorModels.length; i++){
+		monitorModelsDropdown += "<option value ='" + monitorModels[i]+"'>"+ monitorModels[i] +"</option>";
+	}
+	monitorModelsDropdown += "<option value='other'>other</option></select>";
+	
+	//official <select> of printers
+	printerModelsDropdown = "<select name='model' id='model' onchange='ifOther()'>";
+	for (var i = 0; i < printerModels.length; i++){
+		printerModelsDropdown += "<option value ='" + printerModels[i]+"'>"+ printerModels[i] +"</option>";
+	}
+	printerModelsDropdown += "<option value='other'>other</option></select>";
+	
+	//official <select> of network printers
+	netPrinterModelsDropdown = "<select name='model' id='model' onchange ='ifOther()'>";
+	for (var i = 0; i < netPrinterModels.length; i++){
+		netPrinterModelsDropdown += "<option value ='" + netPrinterModels[i]+"'>"+ netPrinterModels[i] +"</option>";
+	}
+	netPrinterModelsDropdown += "<option value='other'>other</option></select>";
+	var scanForm = document.getElementById("scanForm");	
 	var modelInput = "<input id='model' name='model' placeholder='Model #'/>";
 	var assetInput = "<input id='asset' name='asset' placeholder='Asset Tag #'/>";
 	var serialInput = "<input id='serial' name='serial' placeholder='Serial #'/>";
@@ -117,8 +143,6 @@ $location = array("Clerk","Transportation Manager","Maintenance","Maintenance Ma
 	var counter = 1;
 	var submit = document.getElementById("submit");
 	var active = document.activeElement;
-	//var monitorModels = [ "17in", "19in","15in","20in","24in"];
-	//var pcModels = ["Dell Optiplex 790","Dell Optiplex 755", "Dell Optiplex 780", "Dell Latitude E6500", "Dell Latitude D820", "Dell Latitude D830", "Lenovo T430s", "Lenovo T600", "Lenovo T61", "Panasonic Toughbook" ];
 	var html = "";
 	var deviceType = ["monitor","printer"];
 	var deviceSelect ="<select id='deviceSelect' name='deviceSelect'>";
@@ -152,147 +176,93 @@ $location = array("Clerk","Transportation Manager","Maintenance","Maintenance Ma
 	
 	function selectType(){
 	  
-	
-	
-	  //creates form need for what is being input.
-	  if (typeValue.value == "pc"){
-	    var counter = 0;
-	    
-	    scanForm.innerHTML ="";
-	    
-	    //pc
-	    var newModel = "<select id='pcModel' name='pcModel' onchange='ifOtherPC()'>"
-	    for(var i = 0; i < pcModels.length; i++){
-	
-		newModel += "<option value='"+pcModels[i]+"'>"+pcModels[i]+"</options>";
-			
+		//creates form need for what is being input.
+		if (typeValue.value == "pc"){
+			var counter = 0;
+	    		scanForm.innerHTML ="";
+			//pc
+			var newModel = "<select id='pcModel' name='pcModel' onchange='ifOtherPC()'>";
+			for(var i = 0; i < pcModels.length; i++){
+		    	    newModel += "<option value='"+pcModels[i]+"'>"+pcModels[i]+"</options>";
+			}
+			newModel += "<option value='other'>other</option></select><span id='newPCModelSpan'></span>";
+			var newAsset = "<input id='pcAsset' name='pcAsset' placeholder='Asset Tag #'/>";
+			var newSerial = "<input id='pcSerial' name='pcSerial' placeholder='Serial #'/>";
+			scanForm.innerHTML += newModel + newAsset + newSerial + "<br/>" ;
+			counter++;
+			//monitor
+			newModel = "<select id='monitorModel' name='monitorModel' onchange='ifOtherMonitor()'>"
+			for(var i = 0; i < monitorModels.length; i++) {
+			    newModel += "<option value='"+monitorModels[i]+"'>"+ monitorModels[i]+"</options>";			
+			}
+			newModel += "<option value='other'>other</option></select><span id='newMonitorModelSpan'></span>";
+			newAsset = "<input id='monitorAsset' name='monitorAsset' placeholder='Asset Tag #'/>";
+			newSerial = "<input id='monitorSerial' name='monitorSerial' placeholder='Serial #'/>";
+			scanForm.innerHTML += newModel + newAsset + newSerial + "<br/>" ;
+			counter++;
+			//misc 1
+			newModel = "<input id='model3' name='model3' placeholder='Model #'/>";
+			newAsset = "<input id='asset3' name='asset3' placeholder='Asset Tag #'/>";
+			newSerial = "<input id='serial3' name='serial3' placeholder='Serial #'/>";
+			scanForm.innerHTML += deviceSelect + newModel + newAsset + newSerial + "<br/>" ;
+			document.getElementById("deviceSelect").setAttribute("name","misc1");
+			document.getElementById("deviceSelect").setAttribute("id","misc1");
+			counter++;
+			//misc 2
+			newModel = "<input id='model4' name='model4' placeholder='Model #'/>";
+			newAsset = "<input id='asset4' name='asset4' placeholder='Asset Tag #'/>";
+			newSerial = "<input id='serial4' name='serial4' placeholder='Serial #'/>";
+			scanForm.innerHTML += deviceSelect + newModel + newAsset + newSerial + "<br/>" ;
+			document.getElementById("deviceSelect").setAttribute("name","misc2");
+			document.getElementById("deviceSelect").setAttribute("id","misc2");
+			counter++;		  
+			scanForm.firstChild.nextSibling.focus();
+		} else {
+			if (typeValue.value == "monitor"){
+				html = "Please enter monitor's: <select name ='model' id='model' onchange='ifOther()'>";
+				for (var i = 0; i < monitorModels.length; i++){
+					html += "<option value ='" + monitorModels[i]+"'>"+ monitorModels[i] +"</option>";
+				}
+				html += "<option value='other'>other</option></select>";
+				scanForm.innerHTML = html;
+			} else if (typeValue.value == "printer") {
+				html = "Please enter printer's: <select name='model' id='model' onchange='ifOther()'>";
+				for (var i = 0; i < printerModels.length; i++){
+					html += "<option value ='" + printerModels[i]+"'>"+ printerModels[i] +"</option>";
+				}
+				html += "<option value='other'>other</option></select>";
+				scanForm.innerHTML = html;
+			} else {
+				html = "Please enter printer's: <select name='model' id='model' onchange ='ifOther()'>";
+				for (var i = 0; i < netPrinterModels.length; i++){
+					html += "<option value ='" + netPrinterModels[i]+"'>"+ netPrinterModels[i] +"</option>";
+				}
+			  html += "<option value='other'>other</option></select>";
+			  scanForm.innerHTML = html;
+			}
+			scanForm.innerHTML += "<span id='newModelSpan'></span>" + assetInput + serialInput;
+			ifOther();
+			//scanForm.firstChild.focus();
 		}
-		newModel += "<option value='other'>other</option></select><span id='newPCModelSpan'></span>";
-	    var newAsset = "<input id='pcAsset' name='pcAsset' placeholder='Asset Tag #'/>";
-	    var newSerial = "<input id='pcSerial' name='pcSerial' placeholder='Serial #'/>";
-	    scanForm.innerHTML += newModel + newAsset + newSerial + "<br/>" ;
-	    counter++;
-	    //monitor
-	    newModel = "<select id='monitorModel' name='monitorModel' onchange='ifOtherMonitor()'>"
-	    for(var i = 0; i < monitorModels.length; i++) {
-		newModel += "<option value='"+monitorModels[i]+"'>"+ monitorModels[i]+"</options>";			
-		}
-	  newModel += "<option value='other'>other</option></select><span id='newMonitorModelSpan'></span>";
-	  newAsset = "<input id='monitorAsset' name='monitorAsset' placeholder='Asset Tag #'/>";
-	  newSerial = "<input id='monitorSerial' name='monitorSerial' placeholder='Serial #'/>";
-	  scanForm.innerHTML += newModel + newAsset + newSerial + "<br/>" ;
-	  counter++;
-	  //misc 1
-	  newModel = "<input id='model3' name='model3' placeholder='Model #'/>";
-	  newAsset = "<input id='asset3' name='asset3' placeholder='Asset Tag #'/>";
-	  newSerial = "<input id='serial3' name='serial3' placeholder='Serial #'/>";
-	  scanForm.innerHTML += deviceSelect + newModel + newAsset + newSerial + "<br/>" ;
-	  document.getElementById("deviceSelect").setAttribute("name","misc1");
-	  document.getElementById("deviceSelect").setAttribute("id","misc1");
-	  counter++;
-	  //misc 2
-	  newModel = "<input id='model4' name='model4' placeholder='Model #'/>";
-	  newAsset = "<input id='asset4' name='asset4' placeholder='Asset Tag #'/>";
-	  newSerial = "<input id='serial4' name='serial4' placeholder='Serial #'/>";
-	  scanForm.innerHTML += deviceSelect + newModel + newAsset + newSerial + "<br/>" ;
-	  document.getElementById("deviceSelect").setAttribute("name","misc2");
-	  document.getElementById("deviceSelect").setAttribute("id","misc2");
-	  counter++;
-	
-	
-	  
-	    scanForm.firstChild.nextSibling.focus();
-	
-	} else {
-	  if (typeValue.value == "monitor"){
-	    html = "Please enter monitor's: <select name ='model' id='model' onchange='ifOther()'>";
-	    for (var i = 0; i < monitorModels.length; i++){
-		html += "<option value ='" + monitorModels[i]+"'>"+ monitorModels[i] +"</option>";
-	    }
-	    html += "<option value='other'>other</option></select>";
-	    scanForm.innerHTML = html;
-	} else if (typeValue.value == "printer") {
-	    html = "Please enter printer's: <select name='model' id='model' onchange='ifOther()'>";
-	    for (var i = 0; i < printerModels.length; i++){
-		html += "<option value ='" + printerModels[i]+"'>"+ printerModels[i] +"</option>";
-	    }
-	    html += "<option value='other'>other</option></select>";
-	    scanForm.innerHTML = html;
-	  } else {
-		html = "Please enter printer's: <select name='model' id='model' onchange ='ifOther()'>";
-		for (var i = 0; i < netPrinterModels.length; i++){
-		html += "<option value ='" + netPrinterModels[i]+"'>"+ netPrinterModels[i] +"</option>";
-	    }
-	    html += "<option value='other'>other</option></select>";
-	    scanForm.innerHTML = html;
-	  }
-	  
-	  scanForm.innerHTML += "<span id='newModelSpan'></span>" + assetInput + serialInput;
-	  ifOther();
-	  
-	  //scanForm.firstChild.focus();
-	  }
 	}
-	
-	
-	
-	
-	
-	
 	
 	//when pressing enter, focus either moves to the next input, or if the is no more, submits it.
 	function onEnter(){
-	  active = document.activeElement;
-	  if (typeValue.value == "pc") {
-		//create another if statement to see if the set is done.
-	    
-	    
-	   
-	    //document.getElementById("value").innerHTML = active.value + " Last Input: " + lastInput;
-	    if (lastInput == active.value){
-		//when last number is inserted twice as a signal to end the entry, the last entry is omitted then form is submitted.
-		//clear active element, then return true.
-		active.value = "";
-		return true;
-	    
-	    //if the next element is a break...
-	    } else if (active.nextSibling.nodeName == "BR") {
-		//record input in the lastInput variable
-		lastInput = active.value;
-		
-		//if the element after a break is a dropdown menu...
-		active.nextSibling.nextSibling.nextSibling.nextSibling.focus();
-		
-		//user is able to input the next field after pressing enter.
-		return false;
-		//else, if the active element is in "newPCModelSpan"...
-	    } else if (document.activeElement.parentNode == document.getElementById("newPCModelSpan")) {
-		document.getElementById("newPCModelSpan").nextSibling.focus();
-		return false;
-	    } else {
-	      
-	      lastInput = active.value;
-	      active.nextSibling.focus();
-	      
-	      return false;
-	      
-	      
-	    }
-	    
-	    
-	    } else {
-	    if(document.activeElement.nextSibling != document.getElementById("submit")&& document.activeElement.parentNode != document.getElementById("newModelSpan")) {
-	      
-	      document.activeElement.nextSibling.focus();
-	      return false;
-	     
-	    } else if (document.activeElement.parentNode == document.getElementById("newModelSpan")) {
-		document.getElementById("newModelSpan").nextSibling.focus();
-		return false;
-	    } else {
-		return true;
-	    }
-	  }
+		active = document.activeElement;
+		if (typeValue.value == "pc") {
+			if (lastInput == active.value){
+			//when last number is inserted twice as a signal to end the entry,
+			//the last entry is omitted then form is submitted.
+			active.value = "";
+			return true;
+			} else {
+				//record input in the lastInput variable
+				lastInput = active.value;		
+				return moveFocus(active);
+			}
+	    	} else {
+			return moveFocus(active);
+		}
 	}
 	
 	function ifOtherPC() {
@@ -334,9 +304,37 @@ $location = array("Clerk","Transportation Manager","Maintenance","Maintenance Ma
 	
 	
 	
-	//Entry Object
+	//Entry function
 	
+	function entry(device) {
+		var newEntry;
+		//scanForm.innerHTML += <br/> [new form stuff]
+		if (device == "pc") {
+			
+			
+		}
+			//output appropriate form for pc, including dropdown with pcs
+		//if device is monitor
+			//output appropriate from for monitor, including dropdown with monitors
+		//if device is local printer
+			//output appropriate form for local printers, including dropdown menu with local printers
+		//if device is network printer
+			//output appropriate form for network printer, including dropdown menu with network printers
+			
+		//Question is, though, how to give an approriate name for the serverside code to properly insert data into the database
+		//to output, we can either create a new div in the code, and append the code, or use document.write().
+	}
 	
+	function moveFocus(node) {
+		if (node.nextSibling == document.getElementById("submit")){
+			return true;
+		}else if(node.nextSibling.nodeName == "INPUT") {
+			node.nextSibling.focus();
+			return false;
+		} else {
+			return moveFocus(node.nextSibling);
+		}
+	}
 	
 	</script>
 	</body>
