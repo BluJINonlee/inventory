115e548c8f923613fb770c6d401ebc3d83b9d94c
J. Harrison <blujinonlee@gmail.com>
2014-11-02 11:46:25 -0600

    Comments
    Add comments to clarify code in entry.php and updateInventory.php

--- entry.php
+++ entry.php
@@ -1,6 +1,6 @@
 <?php
     class Entry{
-        //initializes all the attributes of an entry
+        // Initialize all the attributes of an entry
         protected $model;
         protected $asset;
         protected $serial;
@@ -12,7 +12,7 @@
         protected $device;
         protected $table;
         
-        
+        // Create constructor
         function __construct($table, $site,$location,$model,$asset,$serial,$lastName,$firstName, $sid) {
             include("connectDatabase.php");
             include_once("ifNull.php");
@@ -30,24 +30,28 @@
             
            
         }
-        
+        // Add a static function to get the Set ID.
         static function getSID() {
             require "connectDatabase.php";
             $row = mysqli_fetch_row(mysqli_query($con, "SELECT MAX(sid) from pcs"));
             return $row[0] + 1;
         }
         
+		// Submits Entry to the database.
         function submit(){
             $con = $this->con;
             $sid = $this->sid;
             
+			// Submit to the Network Printer Table because input fields vary.
             if ($this->table == "netPrinters") {
                 $sql = "INSERT INTO $this->table (asset, serial, model, site, location,lastUpdate)
              VALUES ($this->asset, $this->serial, $this->model,$this->site, $this->location, now());";
             } else {
+			// Handle everything else the same.
                 $sql = "INSERT INTO $this->table (sid, asset, serial, model, lastName, firstName, site, location,lastUpdate)
              VALUES ($this->sid, $this->asset, $this->serial, $this->model,$this->lastName,$this->firstName, $this->site, $this->location, now());";
             }
+			
             echo $sql."<br/>";
             mysqli_autocommit($con,false);
             mysqli_query($con, $sql);
--- updateInventory.php
+++ updateInventory.php
@@ -1,19 +1,32 @@
 <?php
+//start session
 session_start();
+
+//sets session variable that holds the last values entered
 $_SESSION["site"] = $_REQUEST["site"];
 $_SESSION["location"] = ( $_REQUEST["newLocation"] ? $_REQUEST["newLocation"] : $_REQUEST["location"]) ;
-//include_once("connectDatabase.php");
+
+// include_once("connectDatabase.php");
+// Import method to check values for NULL values.
 include_once("ifNull.php");
+
+//
 include_once("entry.php");
 
+// Set session variable to remember last input model.
 if (isset($_REQUEST['model'])) {
 	$_SESSION['model'] = $_REQUEST['model'];
 }
+
+// Lists all request variables for debugging purposes.
 foreach ($_REQUEST as $name => $value){
 	echo $name . ": " . $value. "<br/>";
 
 } 
+// List site in Session variable
 echo "\$_SESSION: ".$_SESSION["site"];
+
+// Declare local variables to hold request variables.
 $sql;
 $asset;
 $serial;
@@ -40,7 +53,7 @@ if ($_REQUEST["scanType"] != "pc"){
 	} else {
 		$model = $_REQUEST["model"];
 	}
-	//determines what table the entry is inserted into
+	// Determine what table the entry is inserted into
 	if ($_REQUEST["scanType"]=="monitor"){
 		$table = "monitors";
 	} else if ($_REQUEST["scanType"]=="printer"){
@@ -48,16 +61,17 @@ if ($_REQUEST["scanType"] != "pc"){
 	} else if ($_REQUEST["scanType"]=="netPrinter"){
 		$table = "netPrinters";
 	}
-	//create new entry class with the $_REQUEST variables
+	// Create new entry class with the $_REQUEST variables. (OOP Implementation)
 	$item = New Entry($table,$site,$location,$model,$asset,$serial,null,null,$sid);
 	$item->submit();
 	
 } else {
-	//if scanType is pc
+	// If scan type is pc, create variables to hold PC and its peripherals
 	$item1;
 	$item2;
 	$item3;
 	$item4;
+	// Create array to hold entry objects created for the set.
 	$items = [];
 	$table = "pcs";
 	$sid = Entry::getSID();
@@ -89,14 +103,14 @@ if ($_REQUEST["scanType"] != "pc"){
 
 
 
-	//inserts pc info into DB
+	// Create new Entry for PC and add to the item array.
 	array_push($items, new Entry($table,$site,$location,$model1,$asset1,$serial1,$lastName,$firstName,$sid));
-	//if any, inserts monititor info into DB
+	// If any, create new Entry for monitors and add to the item array.
 	if ($asset2 != null || $asset2 != "") {
 		$table = "monitors";
 		array_push($items, new Entry($table, $site, $location, $model2, $asset2, $serial2, $lastName, $firstName, $sid));
 	}
-	//if any, inserts first misc item into DB
+	// If any, create new Entry for first extra peripheral and add to the item array.
 	if (($_REQUEST["asset3"] != null || $_REQUEST["asset3"] != "") || ($_REQUEST["serial3"] != null || $_REQUEST["serial3"] != "")) {
 		if($_REQUEST["misc1"] == "monitor"){
 			$table = "Monitors";
@@ -106,7 +120,7 @@ if ($_REQUEST["scanType"] != "pc"){
 		
 		array_push($items, new Entry($table, $site, $location, $model3, $asset3, $serial3, $lastName, $firstName, $sid));
 	}
-	//if any, inserts second misc itme into DB
+	// If any, create new Entry for second extra peripheral and add to the item array.
 	if (($_REQUEST["asset4"] != null || $_REQUEST["asset4"] != "") || ($_REQUEST["serial4"] != null || $_REQUEST["serial4"] != "")) {
 		if($_REQUEST["misc2"] == "monitor"){
 			$table = "Monitors";
@@ -118,9 +132,11 @@ if ($_REQUEST["scanType"] != "pc"){
 }
 
 try{
+	// Submit all items in the $items array.
 	for ($i = 0; $i < sizeof($items); $i++) {
 		$items[$i]->submit();
 	}
+	// Return to the form.
 	header("Location: index.php?$e");
 } catch (Exception $e) {
 	echo $e; 
